1ed48886a01fdb17fc18bb41eaaa6c4286b384cc
MK <kapurmeenakshi@hotmail.com>
2022-10-17 01:46:31 -0700

    commit

---
+++ SeptemberProject/src/SepPackage/SeptemberPractice.java
@@ -0,0 +1,13 @@
+package SepPackage;
+
+public class SeptemberPractice {
+
+	
+	int a=10;
+	public static void main(String[] args) {
+		SeptemberPractice obj = new SeptemberPractice();
+		System.out.println(obj.a);
+		
+	}
+
+}
